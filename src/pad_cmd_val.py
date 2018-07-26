852691c71827b145aab67fbde077070ff40ed5f9
Norihiro02 <s1876011BF@s.chibakoudai.jp>
2018-07-26 22:34:53 +0900

    O

--- src/pad_cmd_val.py
+++ src/pad_cmd_val.py
@@ -3,15 +3,16 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 class pad_cmd_vel:
     def __init__(self):
+        #init node
         rospy.init_node('pad_cmd_vel')
         self.joy_sub=rospy.Subscriber('joy',Joy,self.joy_callback,queue_size=1)
         self.twist_pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
         rospy.spin()
     def joy_callback(self,joy_msg):
         twist=Twist();
+        #make cmd_vel parameter
         twist.linear.x=joy_msg.axes[1]
         twist.linear.y=joy_msg.axes[0]
-
         twist.angular.z=joy_msg.axes[3]
 
         self.twist_pub.publish(twist)
